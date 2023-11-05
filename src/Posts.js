0f471736f1e20dbd9898c63fd31d36397ea72283
BrayanCode11 <bangarita@unal.edu.co>
2023-11-05 14:19:25 -0500

    Add react-icons package and update Posts component

---
+++ src/Posts.js
@@ -0,0 +1,17 @@
+import { VscArrowCircleDown , VscAccount} from "react-icons/vsc";
+
+export const Posts = () => {
+  return (
+    <button
+      onClick={() => {
+        fetch("https://jsonplaceholder.typicode.com/posts")
+          .then((response) => response.json())
+          .then((data) => console.log(data))
+          .catch((error) => console.log(error));
+      }}
+    >
+      <VscAccount/>  
+      Traer datos
+    </button>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Greeting, UserCard } from "./Greeting";
-import Product, { Navbar } from "./Product";
-import { Button } from "./Button";
-import {TaskCard} from './Task';
-import {Saludar} from './Saludar';
+import { Posts } from "./Posts";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render( 
+root.render(
   <>
-  <TaskCard/>
-  <Button text="Medina es gay?"/>
+    <Posts />
   </>
 );
